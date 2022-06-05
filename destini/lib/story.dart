3a010fd87e96923604cd9afa8e2b3189adab7766
Diego Carlino <diego_carlino@outlook.com>
2022-06-05 13:36:58 +0200

    destini

---
+++ destini/lib/story.dart
@@ -0,0 +1,11 @@
+class Story {
+  late final String storyTitle;
+  late final String choice1;
+  late final String choice2;
+
+  Story({
+    required this.storyTitle,
+    required this.choice1,
+    required this.choice2,
+  });
+}
